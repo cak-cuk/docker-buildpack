9bb4a4d06757603e1525caff7c8ecb92925314cb
Mahyuddin Susanto <udienz@gmail.com>
2021-12-29 15:18:30 +0700

    Add github actions
     - .github/workflows/*.yml: Add docker build for github action
     - utils/gen-github-workflow.sh: add script to generate github action

---
+++ utils/gen-github-workflow.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x 
+BASE=$CI_PROJECT_DIR
+DISTRO="centos7 centos8 trusty xenial bionic focal jessie stretch buster alpine-edge alpine-latest"
+# OUT=../.gitlab-ci.yml
+
+for x in $DISTRO
+	do
+	OUT=.github/workflows/$x.yml
+  echo $x
+  echo "name: Build $x Docker
+on: [push]
+jobs:
+  build:
+    runs-on: [self-hosted]
+    steps:
+    - uses: actions/checkout@master
+    
+    - name: Publish to Registry
+      uses: elgohr/Publish-Docker-Github-Action@master
+      with:
+        name: udienz/buildpack
+        username: \${{ github.actor }}
+        password: \${{ secrets.DOCKER_PASSWORD }}
+        dockerfile: focal/Dockerfile
+        buildoptions: \"--compress --force-rm\"
+        tags: \"$x\"
+" > $OUT
+done
